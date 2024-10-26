82d30e9309e2f4e65624ed5df2518bdf22c5b1eb
Awel <87609387+Invisgg@users.noreply.github.com>
2024-10-26 12:14:25 -0500

    N/A

--- src/main/java/rip/hardcore/basic/Basic.java
+++ src/main/java/rip/hardcore/basic/Basic.java
@@ -2,15 +2,24 @@ package rip.hardcore.basic;
 
 import co.aikar.commands.PaperCommandManager;
 import org.bukkit.Bukkit;
+import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
 import rip.hardcore.basic.commands.BroadcastCommand;
 import rip.hardcore.basic.commands.DiscordCommand;
 import rip.hardcore.basic.commands.StatsCommand;
+import rip.hardcore.basic.commands.Warps.SetSpawnCommand;
+import rip.hardcore.basic.commands.Warps.SpawnCommand;
+import rip.hardcore.basic.commands.Warps.WarpManager;
+import rip.hardcore.basic.commands.Warps.WarpRequester;
 import rip.hardcore.basic.listeners.General;
 import rip.hardcore.basic.listeners.OnFirstJoin;
 import rip.hardcore.basic.listeners.PlayerDeath;
 import rip.hardcore.basic.manager.LifeManager;
 import rip.hardcore.basic.commands.LivesCommand;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import rip.hardcore.basic.manager.LocationManager;
+import rip.hardcore.basic.storage.Warps;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +27,10 @@ import java.io.IOException;
 public final class Basic extends JavaPlugin {
 
     private LifeManager lifeManager;
+    Warps warps;
+    WarpManager warpManager;
+    WarpRequester warpRequester;
+    LocationManager locationManager;
 
     @Override
     public void onEnable() {
@@ -34,6 +47,7 @@ public final class Basic extends JavaPlugin {
             }
         }
         lifeManager = new LifeManager(dbFile);
+        YAML();
         Commands();
 
         getServer().getPluginManager().registerEvents(new PlayerDeath(lifeManager), this);
@@ -50,6 +64,28 @@ public final class Basic extends JavaPlugin {
         commandManager.registerCommand(new BroadcastCommand());
         commandManager.registerCommand(new LivesCommand(lifeManager));
         commandManager.registerCommand(new StatsCommand(lifeManager));
+        commandManager.registerCommand(new SpawnCommand(warpRequester,  locationManager));
+        commandManager.registerCommand(new SetSpawnCommand(locationManager, warps));
+
+        Bukkit.getPluginManager().addPermission(new Permission("basic.auto", "Automaticaly teleport with warps", PermissionDefault.OP));
+    }
+
+    private void YAML(){
+        try {
+            File configFiles = new File(getDataFolder(), "warps.yml");
+            if (!configFiles.exists()) {
+                saveResource("warps.yml", false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            getLogger().severe("Failed to load warps file in start up");
+            getLogger().severe("Auto disabling Vandal... make a ticket in discord");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+        this.warps = new Warps(this);
+        warpManager = new WarpManager();
+        warpRequester = new WarpRequester(warpManager);
+        locationManager = new LocationManager(warps);
     }
 
     private void Listener(){
