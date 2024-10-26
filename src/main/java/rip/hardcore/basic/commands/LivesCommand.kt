68413dfa4c13f65ed4107b676cc9abe96c5ae9ca
Awel <87609387+Invisgg@users.noreply.github.com>
2024-10-25 23:56:42 -0500

    Changes by snowy

--- src/main/java/rip/hardcore/basic/commands/LivesCommand.kt
+++ src/main/java/rip/hardcore/basic/commands/LivesCommand.kt
@@ -1,6 +1,6 @@
 package rip.hardcore.basic.commands
 
-// [@] This file is authored by SnowyJS on 10/12/2024
+// [@] This file is authored by SnowyJS on 10/25/2024
 // [@] https://snowyjs.lol
 // [@] https://github.com/snowypy
 
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 import rip.hardcore.basic.manager.LifeManager
+import rip.hardcore.filter.util.translate
 
 @CommandAlias("lives")
 class LivesCommand(private val lifeManager: LifeManager) : BaseCommand() {
@@ -23,15 +24,15 @@ class LivesCommand(private val lifeManager: LifeManager) : BaseCommand() {
         }
 
         if (targetPlayer == null) {
-            sender.sendMessage("&#FF0000Player not found.")
+            sender.sendMessage("&#FF0000Player not found.".translate())
             return
         }
 
         val lives = lifeManager.getLives(targetPlayer.uniqueId)
         if (targetPlayer == sender) {
-            sender.sendMessage("&aYou have $lives lives remaining.")
+            sender.sendMessage("&aYou have $lives lives remaining.".translate())
         } else {
-            sender.sendMessage("&a${targetPlayer.name} has $lives lives remaining.")
+            sender.sendMessage("&a${targetPlayer.name} has $lives lives remaining.".translate())
         }
     }
 
@@ -42,18 +43,18 @@ class LivesCommand(private val lifeManager: LifeManager) : BaseCommand() {
         val targetPlayer = Bukkit.getPlayerExact(targetName)
 
         if (targetPlayer == null) {
-            sender.sendMessage("&#FF0000Player not found.")
+            sender.sendMessage("&#FF0000Player not found.".translate())
             return
         }
 
         if (amount <= 0) {
-            sender.sendMessage("&#FF0000You must send a positive number of lives.")
+            sender.sendMessage("&#FF0000You must send a positive number of lives.".translate())
             return
         }
 
         val senderLives = lifeManager.getLives(sender.uniqueId)
         if (senderLives < amount) {
-            sender.sendMessage("&#FF0000You don't have enough lives to give.")
+            sender.sendMessage("&#FF0000You don't have enough lives to give.".translate())
             return
         }
 
@@ -61,7 +62,7 @@ class LivesCommand(private val lifeManager: LifeManager) : BaseCommand() {
         val targetLives = lifeManager.getLives(targetPlayer.uniqueId)
         lifeManager.setLives(targetPlayer.uniqueId, targetLives + amount)
 
-        sender.sendMessage("&aYou gave $amount lives to ${targetPlayer.name}. You now have ${senderLives - amount} lives.")
-        targetPlayer.sendMessage("&aYou received $amount lives from ${sender.name}. You now have ${targetLives + amount} lives.")
+        sender.sendMessage("&aYou gave $amount lives to ${targetPlayer.name}. You now have ${senderLives - amount} lives.".translate())
+        targetPlayer.sendMessage("&aYou received $amount lives from ${sender.name}. You now have ${targetLives + amount} lives.".translate())
     }
 }
