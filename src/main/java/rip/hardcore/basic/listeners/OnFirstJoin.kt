77f4f17f9c2af1ad7d176c48fc42214470c4ae67
Awel <87609387+Invisgg@users.noreply.github.com>
2024-10-25 23:56:46 -0500

    Changes by snowy

--- src/main/java/rip/hardcore/basic/listeners/OnFirstJoin.kt
+++ src/main/java/rip/hardcore/basic/listeners/OnFirstJoin.kt
@@ -1,14 +1,16 @@
 package rip.hardcore.basic.listeners
 
-// [@] This file is authored by SnowyJS on 10/12/2024
+// [@] This file is authored by SnowyJS on 10/25/2024
 // [@] https://snowyjs.lol
 // [@] https://github.com/snowypy
 
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
 import rip.hardcore.basic.manager.LifeManager
+import rip.hardcore.filter.util.translate
 
 class OnFirstJoin(private val lifeManager: LifeManager) : Listener {
 
@@ -17,8 +19,10 @@ class OnFirstJoin(private val lifeManager: LifeManager) : Listener {
         val player = event.player
         val playerUUID = player.uniqueId
 
+
         if (lifeManager.isBanned(playerUUID)) {
-            player.kickPlayer("&#FF0000&lYOU ARE BANNED &7- &fYou lost all of your lives so you are banned. This will expire 3 hours from your death.")
+            player.kickPlayer("&#FF0000&lYOU ARE BANNED &7- &fYou lost all of your lives so you are banned. This will expire 3 hours from your death.".translate())
         }
     }
+
 }
