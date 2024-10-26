5b7c44ffaefae9f2ff49266f3a1b6a8dfc00de2e
Awel <87609387+Invisgg@users.noreply.github.com>
2024-10-25 23:55:42 -0500

    N/A

--- src/main/java/rip/hardcore/basic/Basic.java
+++ src/main/java/rip/hardcore/basic/Basic.java
@@ -3,6 +3,10 @@ package rip.hardcore.basic;
 import co.aikar.commands.PaperCommandManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import rip.hardcore.basic.commands.BroadcastCommand;
+import rip.hardcore.basic.commands.DiscordCommand;
+import rip.hardcore.basic.commands.StatsCommand;
+import rip.hardcore.basic.listeners.General;
 import rip.hardcore.basic.listeners.OnFirstJoin;
 import rip.hardcore.basic.listeners.PlayerDeath;
 import rip.hardcore.basic.manager.LifeManager;
@@ -30,8 +34,8 @@ public final class Basic extends JavaPlugin {
                 return;
             }
         }
-
         lifeManager = new LifeManager(dbFile);
+        Commands();
 
         getServer().getPluginManager().registerEvents(new PlayerDeath(lifeManager), this);
         getServer().getPluginManager().registerEvents(new OnFirstJoin(lifeManager), this);
@@ -44,7 +48,10 @@ public final class Basic extends JavaPlugin {
 
     private void Commands(){
         PaperCommandManager commandManager = new PaperCommandManager(this);
+        commandManager.registerCommand(new DiscordCommand());
+        commandManager.registerCommand(new BroadcastCommand());
         commandManager.registerCommand(new LivesCommand(lifeManager));
+        commandManager.registerCommand(new StatsCommand(lifeManager));
     }
 
     private void Listener(){
