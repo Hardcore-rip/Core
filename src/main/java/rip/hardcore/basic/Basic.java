78e6f4eb966c2ebb9551afd1060f825c84c824a4
Awel <87609387+Invisgg@users.noreply.github.com>
2024-10-26 21:11:15 -0500

    Homes

--- src/main/java/rip/hardcore/basic/Basic.java
+++ src/main/java/rip/hardcore/basic/Basic.java
@@ -1,6 +1,7 @@
 package rip.hardcore.basic;
 
 import co.aikar.commands.PaperCommandManager;
+import com.samjakob.spigui.SpiGUI;
 import org.bukkit.Bukkit;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,6 +14,8 @@ import rip.hardcore.basic.commands.Warps.WarpManager;
 import rip.hardcore.basic.commands.Warps.WarpRequester;
 import rip.hardcore.basic.commands.homes.AdminHomeCommand;
 import rip.hardcore.basic.commands.homes.HomeCommand;
+import rip.hardcore.basic.commands.tags.TagCommand;
+import rip.hardcore.basic.commands.tags.TagMenu;
 import rip.hardcore.basic.listeners.General;
 import rip.hardcore.basic.listeners.OnFirstJoin;
 import rip.hardcore.basic.listeners.PlayerDeath;
@@ -21,10 +24,14 @@ import rip.hardcore.basic.manager.LifeManager;
 import rip.hardcore.basic.commands.LivesCommand;
 import org.bukkit.permissions.Permission;
 import rip.hardcore.basic.manager.LocationManager;
+import rip.hardcore.basic.manager.TagManager;
+import rip.hardcore.basic.storage.Tags;
 import rip.hardcore.basic.storage.Warps;
+import rip.hardcore.basic.utils.AnvilGUI;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public final class Basic extends JavaPlugin {
 
@@ -34,6 +41,9 @@ public final class Basic extends JavaPlugin {
     WarpRequester warpRequester;
     private HomeManager homeManager;
     LocationManager locationManager;
+    TagManager tagManager;
+    Tags tags;
+    public static SpiGUI spiGUI;
 
     @Override
     public void onEnable() {
@@ -49,6 +59,38 @@ public final class Basic extends JavaPlugin {
                 return;
             }
         }
+
+        try {
+            File configFiles = new File(getDataFolder(), "tags.json");
+            if (!configFiles.exists()) {
+                saveResource("tags.json", false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            getLogger().severe("Failed to load tags file in start up");
+            getLogger().severe("Auto disabling Basic... make a ticket in discord");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+        tags = new Tags(this);
+
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdir();
+            }
+
+            tagManager = new TagManager(getDataFolder().getAbsolutePath() + "/tags.db");
+            if (tagManager != null) {
+                getLogger().info("Tag data initialized successfully.");
+            } else {
+                getLogger().severe("Tag data initialization failed.");
+                Bukkit.getPluginManager().disablePlugin(this);
+            }
+        } catch (Exception e) {
+            getLogger().severe("Player data initialization failed.");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+
+        spiGUI = new SpiGUI(this);
         lifeManager = new LifeManager(dbFile);
         homeManager = new HomeManager();
         YAML();
@@ -68,10 +110,11 @@ public final class Basic extends JavaPlugin {
         commandManager.registerCommand(new BroadcastCommand());
         commandManager.registerCommand(new LivesCommand(lifeManager));
         commandManager.registerCommand(new StatsCommand(lifeManager));
-        commandManager.registerCommand(new SpawnCommand(warpRequester,  locationManager));
+        commandManager.registerCommand(new SpawnCommand(warpRequester,  locationManager, warps));
         commandManager.registerCommand(new SetSpawnCommand(locationManager, warps));
         commandManager.registerCommand(new HomeCommand(homeManager));
         commandManager.registerCommand(new AdminHomeCommand(homeManager));
+        commandManager.registerCommand(new TagCommand(tagManager, new TagMenu(tagManager, tags), tags));
 
         Bukkit.getPluginManager().addPermission(new Permission("basic.auto", "Automaticaly teleport with warps", PermissionDefault.OP));
     }
